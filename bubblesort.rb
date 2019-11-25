ec54751514b3147148eb9c152bb16fadd5154f83
Eduardo Bonilla <eduardobonillac@gmail.com>
2019-11-25 11:54:03 -0600

    bubblesortby method

--- bubblesort.rb
+++ bubblesort.rb
@@ -11,6 +11,26 @@ def bubble_sort (arr)
   puts arr
 end
 
+def bubble_sort_by (arr)
+  arr.each do |x|
+    arr.each_with_index do |value,key|
+      if key != (arr.length()-1)
+        if yield(value, arr[key+1]) > 0
+          arr[key],arr[key+1] = arr[key+1],arr[key]
+        end
+      end
+    end
+  end
+  puts arr
+end
+
 begin
-puts bubble_sort([3,2,1]);
+
+testa = [3,2,1,6,9,1,2,4,6,5,3,10,1,2]
+testb = ["hi","hello","hey","ko","have","a","nice","day"]
+puts bubble_sort(testa);
+bubble_sort_by(testb) do |left,right|
+  left.length - right.length
+end
+
 end
