391ed07769e776b846ea86361ec57ed5900bd865
Eduardo Bonilla <eduardobonillac@gmail.com>
2019-11-25 10:26:05 -0600

    bubble sort fisrt implementation

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,1 +1,11 @@
-puts "hello world"
+def bubble_sort (arr)
+  arr.each_with_index do |value,key|
+    if value > arr[key.next]
+      value,arr[key.next] = arr[key.next],value
+    end
+  end
+  return arr
+end
+begin
+puts bubble_sort([2,6,8,3,5,9,12,1,3,2]);
+end
